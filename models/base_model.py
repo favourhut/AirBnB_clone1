297150a3e300ffea3970a5ed35e594836276eb95
Favour <135041430+favourhut@users.noreply.github.com>
2024-05-15 02:41:37 +0100

    Created BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+    """_summary_
+    """
+    
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        
+        
+    # Printing information for the instance attributes 
+    def __str__(self):
+        
+        """should return [<class name>] (<self.id>) <self.__dict__>"""
+        return(self.__class__.__name__, self.id, self.__dict__)
+    
+    # creating some public instances
+    
+    def save(self):
+        """ makes updated_at valid to current datetime"""
+        updated_at = datetime.now()
+        
+    def to_dict(self):
+        """creating an empty dict"""
+        all_keys_values_dict = {}
+        
+        """ Using self.__dict__ to retun only only sets instances"""
+        all_keys_values_dict.update(self.__dict__)
+        
+        """Adding a key __class__ with class name of the object"""
+        all_keys_values_dict["__class__"] = self.__class__.__name__
+        
+        """Converting created_at and updated_at to isoformat"""
+        all_keys_values_dict["created_at"] = all_keys_values_dict["created_at"].isoformat()
+        all_keys_values_dict["update_at"] = all_keys_values_dict["update_at"].isoformat()
+        
+        """returning the dictionary containing all keys and values"""
+        return (all_keys_values_dict)
