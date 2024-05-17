3346ec2d0369fe19eae7f87e85808ebdab4ecc33
Favour <135041430+favourhut@users.noreply.github.com>
2024-05-17 13:50:23 +0100

    Amenity

---
+++ amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""class that inherits from basemodel"""
+
+from base_model import BaseModel
+
+class Amenity(BaseModel):
+    
+    """public class attributes
+
+    Args:
+        name: empty string
+    """
+    name = ""
