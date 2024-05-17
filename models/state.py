908b1103c8be6055efae61a3f5e0b72d2630ac76
Favour <135041430+favourhut@users.noreply.github.com>
2024-05-17 13:52:40 +0100

    class State

---
+++ models/state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Inhresits fom BaseModel"""
+
+from base_model import BaseModel
+
+
+class State(BaseModel):
+    
+    """A public class attribute
+    
+    Attributes:
+        name: empty string
+    """
+    name = ""
