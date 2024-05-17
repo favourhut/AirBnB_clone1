b29612c2d6ddf488fbaeaf88aa6292e87e49dd7a
Favour <135041430+favourhut@users.noreply.github.com>
2024-05-17 01:58:43 +0100

    Created BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,21 @@ class BaseModel():
     """_summary_
     """
     
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
         
+        """Creating a basemodel from dictionary"""
+        
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key == "__class__":
+                    continue
+                setattr(self, key, value)
+            else:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
         
     # Printing information for the instance attributes 
     def __str__(self):
