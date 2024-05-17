690f3a5bd3938d69c9746e59393b2cb33b938fc0
Favour <135041430+favourhut@users.noreply.github.com>
2024-05-17 14:07:09 +0100

    Created the class Place

--- models/place.py
+++ models/place.py
@@ -4,7 +4,24 @@
 
 from base_model import BaseModel
 
+
 class Place(BaseModel):
+        
+    """Inherits from BaseModel class
+
+    Attributes:
+        city_id (str): City id
+        user_id (str): User id
+        name (str): name of the place.
+        description (str): description of the place
+        number_rooms (int): number of rooms of the place
+        number_bathrooms (int): number of bathrooms of the place
+        max_guest (int): maximum number of guests of the place
+        price_by_night (int): price by night of the place
+        latitude (float): latitude of the place
+        longitude (float): longitude of the place
+        amenity_ids (list): list of Amenity ids
+    """
     
     city_id = ""
     user_id = ""
